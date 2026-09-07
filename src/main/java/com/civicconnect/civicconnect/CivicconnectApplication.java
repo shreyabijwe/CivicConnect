923078f5d38909941a9c742a42cc80b69cb2d60e
Shreya Bijwe <shreyabijwe52@gmail.com>
2026-09-08 00:08:24 +0530

    Initial Spring Boot setup with MySQL connection
    Initial Spring Boot setup with MySQL connection

---
+++ src/main/java/com/civicconnect/civicconnect/CivicconnectApplication.java
@@ -0,0 +1,13 @@
+package com.civicconnect.civicconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/civicconnect/civicconnect/CivicconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicconnect.civicconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
